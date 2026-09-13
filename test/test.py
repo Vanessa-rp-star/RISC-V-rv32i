ff1de3e0bf7b3499ad7d46b3c712d69ba5ad1c2d
Vanessa RP <vanessa.perez10@uabc.edu.mx>
2026-09-12 23:03:29 -0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,3 +1,4 @@
+
 PROGRAM_A = [
     0x00500093, 0x00A00113, 0x002081B3, 0x40110233, 0x00000293, 0x0032A023, 0x0002A303, 0x04000513,
     0x00652023, 0x00052603, 0x00167613, 0xFE061CE3, 0x04800593, 0x0AA00713, 0x00E5A023, 0x0005A783,
@@ -16,12 +17,11 @@ PROGRAM_D = [
     0x00000F13, 0x01EA2023, 0x000A2483, 0x0014F493, 0xFE049CE3, 0x00000063, 0x00000013, 0x00000013,
 ]
  
- 
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles, Timer, RisingEdge
  
-CLK_FREQ_HZ = 20_000_000     # frecuencia real del chip (ver info.yaml/config.json)
+CLK_FREQ_HZ = 10_000_000     # frecuencia real del chip (ver info.yaml/config.json)
 BAUD_RATE   = 115200
 CLK_PERIOD_NS = round(1e9 / CLK_FREQ_HZ)
 CLKS_PER_BIT  = CLK_FREQ_HZ // BAUD_RATE
@@ -140,4 +140,3 @@ async def test_program_d_branches_jumps(dut):
  
     result = await uart_recv_byte(dut)
     assert result == 0x01, f"esperaba 0x01 (todo paso), llego 0x{result:02X}"
- 
